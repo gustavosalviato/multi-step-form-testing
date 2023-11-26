ff876c0620bdace8887a9611f1db9657ad8afaa3
gustavosalviato <gustavosalviato8@gmail.com>
2023-11-26 09:16:20 -0300

    chore: font config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black">
-      <div>...</div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black px-4">
+      <div className="flex flex-col space-y-4 rounded-sm p-6 bg-white max-w-2xl w-full">
+        <h2 className="text-center items-center text-xl font-bold">Contact</h2>
+      </div>
     </div>
   )
 }
